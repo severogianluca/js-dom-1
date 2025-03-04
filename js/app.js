3c6cf4cc26d4e56ad994b47cd542ed78ca29df3b
severogianluca <gianlucasevero300@gmail.com>
2025-03-04 17:41:51 +0100

    Feat: create bulb-on

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log('salve buonasera')
+// console.log('salve buonasera')
+
+const bulbOff = document.getElementById('bulb-off');
+const buttonOn = document.getElementById('button-on')
+
+buttonOn.addEventListener('click', function () {
+    bulbOff.src = '../img/yellow_lamp.png'
+})
+
