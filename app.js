3e128fcf3f380af46623056427234957f491114c
severogianluca <gianlucasevero300@gmail.com>
2025-03-04 18:16:12 +0100

    Feat: create bonus bulb

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ ESERRCIZIO:
 
 BONUS:
 buttonOn.addEventListener('click', function () {
-    if(bulbOff.src.includes('img/white_lamp.png')){
-        bulbOff.src = 'img/yellow_lamp.png';
+    if(bulbOff.src.includes('/img/white_lamp.png')){
+        bulbOff.src = '../img/yellow_lamp.png';
         buttonOn.innerText = 'SPEGNI';
         console.log('acceso if')
         
     }else{
-        bulbOff.src = 'img/white_lamp.png';
+        bulbOff.src = '/img/white_lamp.png';
         buttonOn.innerText = 'ACCENDI';
         console.log('spento else')
     }
